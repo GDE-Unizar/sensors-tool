127abbd7018b76c8fabb0d114408effde6cb422b
Abel <544125@unizar.es>
2020-08-21 19:52:55 +0200

    on/off vibrator

--- app/src/main/java/com/unizar/practica/fragment/Vibrator.kt
+++ app/src/main/java/com/unizar/practica/fragment/Vibrator.kt
@@ -5,19 +5,19 @@ import android.os.Vibrator
 import com.unizar.practica.MainActivity
 import com.unizar.practica.tools.Fragment
 import com.unizar.practica.tools.onCheckedChange
-import com.unizar.practica.tools.onProgressChange
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Makes the device vibrate
  */
 class Vibrator(
-        val cntx: MainActivity
+        val cntx: MainActivity,
 ) : Fragment {
 
     // variables
     private lateinit var vibrator: Vibrator
-    private val vibconfig = longArrayOf(0, 0)
+
+    private val vibconfig = longArrayOf(0, Integer.MAX_VALUE.toLong())
 
     /**
      * Initialization
@@ -27,44 +27,16 @@ class Vibrator(
         vibrator = cntx.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
         // toggle to start/stop
-        cntx.vib_tog.onCheckedChange { update() }
-
-        // on time
-        cntx.vib_on.run {
-            onProgressChange { value -> updateProgressBar(0, value) }
-            progress = max / 2
-        }
-
-        // off time
-        cntx.vib_off.run {
-            onProgressChange { value -> updateProgressBar(1, value) }
-            progress = max / 2
-        }
-
-        // init update
-        update()
-    }
-
-    /**
-     * When a progress bar changes, update the array value
-     */
-    fun updateProgressBar(id: Int, progress: Int) {
-        vibconfig[id] = progress.toLong()
-        update()
+        cntx.vib_tog.onCheckedChange { setState(it) }
     }
 
     /**
      * Update ui (text)
      */
-    private fun update() {
-        // set label
-        cntx.vib_tog.text = "off=${vibconfig[0]} - on=${vibconfig[1]}"
-
-        // check 0,0
-        if (cntx.vib_tog.isChecked && vibconfig.maxOrNull() ?: 0 <= 0) cntx.vib_tog.isChecked = false
+    private fun setState(state: Boolean) {
 
         // start/stop
-        if (cntx.vib_tog.isChecked) vibrator.vibrate(vibconfig, 0)
+        if (state) vibrator.vibrate(vibconfig, 0)
         else vibrator.cancel()
     }
 
