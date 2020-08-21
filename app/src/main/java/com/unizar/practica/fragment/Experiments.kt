22ba2d261d019cb77cd0145c61cfd59d116d98cb
Abel <544125@unizar.es>
2020-08-21 18:30:49 +0200

    ability to stop experiments

--- app/src/main/java/com/unizar/practica/fragment/Experiments.kt
+++ app/src/main/java/com/unizar/practica/fragment/Experiments.kt
@@ -1,6 +1,7 @@
 package com.unizar.practica.fragment
 
 import android.widget.Button
+import android.widget.Toast
 import com.unizar.practica.MainActivity
 import com.unizar.practica.tools.FileWriter
 import com.unizar.practica.tools.Volume
@@ -21,36 +22,64 @@ class Experiments(
 
     // utils
     val file = FileWriter(cntx, "exp")
-
     val experiments = sequenceOf(
             "Frequency" to ::freqGraph,
             "Volume" to ::volumeGraph,
     )
 
+    // variables
+    var current: Thread? = null
+        set(value) {
+            field = value
+            btn_stop.isEnabled = value != null
+            btn_exps.forEach { it.isEnabled = value == null }
+        }
+    lateinit var btn_stop: Button
+    val btn_exps = ArrayList<Button>()
+
     /**
      * When created
      */
     fun onCreate() {
+
         // add a button foreach experiment
         experiments.forEach { (name, function) ->
             Button(cntx).apply {
                 text = "Experiment:\n$name"
                 setOnClickListener {
-                    thread {
-                        // wait before
-                        sleep(10000)
-                        // initialize
-                        screen { cntx.showAll() }
-                        file.openNew(name)
-                        // run after
-                        function()
+                    current?.interrupt()
+                    current = thread {
+                        screen { cntx.hideAll() }
+                        try {
+                            // wait before
+                            screen { Toast.makeText(cntx, "Initiating experiment in 5 seconds...", Toast.LENGTH_SHORT).show() }
+                            sleep(5_000)
+                            // initialize
+                            screen { cntx.showAll() }
+                            file.openNew(name)
+                            // run
+                            function()
+                        } catch (ignored: InterruptedException) {
+                        }
                         // finalize
                         screen { file.close() }
                         screen { cntx.hideAll() }
                     }
                 }
                 cntx.main.addView(this)
+                btn_exps.add(this)
             }
+        }
+
+        // add stop button
+        btn_stop = Button(cntx).apply {
+            text = "Stop experiment"
+            isEnabled = false
+            setOnClickListener {
+                current?.interrupt()
+                current = null
+            }
+            cntx.main.addView(this)
         }
     }
 
