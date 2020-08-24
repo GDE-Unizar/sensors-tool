b6586366bbbbff9f60bafc7f2f7171c728392886
Abel <544125@unizar.es>
2020-08-24 17:48:41 +0200

    buttons with icons

---
+++ app/src/main/java/com/unizar/practica/tools/ToggleImageButton.kt
@@ -0,0 +1,48 @@
+package com.unizar.practica.tools
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageButton
+import com.unizar.practica.R
+
+/**
+ * There is ToggleButton, and ImageButton, but no ToggleImageButton.
+ * An ImageButton with a checked state.
+ * Hardcoded images
+ */
+class ToggleImageButton(context: Context?, attrs: AttributeSet?) : ImageButton(context, attrs) {
+
+    private var listener: ((Boolean) -> Unit)? = null
+
+    /**
+     * The checked state. Getter and setter
+     */
+    var isChecked = false
+        set(value) {
+            field = value
+            update()
+        }
+
+    /**
+     * Initialization
+     */
+    init {
+        setOnClickListener { isChecked = !isChecked }
+        update()
+    }
+
+    /**
+     * Set listener for checked change
+     */
+    fun onCheckedChange(listener: (Boolean) -> Unit) {
+        this.listener = listener
+    }
+
+    /**
+     * Internal update
+     */
+    private fun update() {
+        setImageResource(if (isChecked) R.drawable.ic_stop else R.drawable.ic_record)
+        listener?.invoke(isChecked)
+    }
+}
